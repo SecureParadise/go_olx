08e97dae99fe42a78ee9f5185a43c232ecb26ea3
SecureParadise <amrichthakur@proton.me>
2026-08-12 21:43:19 +0545

    prod test2

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -21,9 +21,7 @@ func main() {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		// strictly follow chronollogy and order
-		w.Write([]byte(`{
-		"status" : "sab gazab"
-		}`))
+		w.Write([]byte(`{"status":"okey rey"}`))
 	})
 
 	server := &http.Server{
